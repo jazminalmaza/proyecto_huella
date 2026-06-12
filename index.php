91bc9cd6f7d649d21a3be7b426b8d35bbaf0db17
unknown <jazminalmaza09@gmail.com>
2026-06-12 16:05:05 -0300

    Editando index otra vez

---
+++ index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <meta charset="UTF-8">
+    <title>Sistema de Asistencia</title>
+    <link rel="stylesheet" href="estilo.css">
+</head>
+
+<body>
+
+<div class="menu">
+
+    <h1>Sistema de Control de Asistencia</h1>
+
+    <a href="asistencia.php">
+        Registrar Asistencia
+    </a>
+
+    <a href="ver_asistencias.php">
+        Ver Asistencias
+    </a>
+
+    <a href="docentes.php">
+        Docentes
+    </a>
+
+</div>
+
+</body>
+
+</html>
